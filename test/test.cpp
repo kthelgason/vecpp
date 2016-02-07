d96d5125a79208c575c89f316fd007483d852b49
Kári Tristan Helgason <kthelgason@gmail.com>
2016-02-07 21:35:22 +0000

    Adds tests for zipWith

--- test/test.cpp
+++ test/test.cpp
@@ -22,6 +22,15 @@ TEST(append, success) {
     EXPECT_EQ(v3.at<4>(), 'e');
 }
 
+TEST(zipWith, default) {
+    Vec<int, 3> v1 = {1, 2, 3};
+    Vec<int, 3> v2 = {1, 2, 3};
+    std::function<int(int,int)> fun = [](int a, int b) { return a * b; };
+    auto v3 =  v1.zipWith(v2, fun);
+    Vec<int, 3> v4 = {1, 4, 9};
+    EXPECT_EQ(v3, v4);
+}
+
 GTEST_API_ int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
